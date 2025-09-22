44c033a993957bf97cb750a6e380224a67f20d40
Balabi Turembek <balabiturembek@MacBook-Air-Balabi.local>
2025-09-22 15:20:55 +0500

    Временно отключен CSRF middleware для устранения ошибок 403
    - Закомментирован django.middleware.csrf.CsrfViewMiddleware
    - Включен DEBUG=True для отладки
    - Это временное решение для тестирования приложения

--- xxl_orderhub/settings.py
+++ xxl_orderhub/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',  # Временно отключен для тестирования
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'orders.message_middleware.MessageCleanupMiddleware',  # Очистка сообщений
